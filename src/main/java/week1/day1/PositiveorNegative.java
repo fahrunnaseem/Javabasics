823ad285f01a65df3e4ebe3d7e6a67e58b9be258
91967 <91967@Nazeem>
2022-04-17 01:13:01 +0530

    Second commit

--- src/main/java/week1/day1/PositiveorNegative.java
+++ src/main/java/week1/day1/PositiveorNegative.java
@@ -5,7 +5,7 @@ public class PositiveorNegative {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int number = 35;
+		int number = -35;
 		if(number>=0) {
 			System.out.println("The number is positive");
 		}else
